ab96d1ad7fca65138c88af5c36aacffeea5fc8e9
NilaAyu1611 <nilaayucw@gmail.com>
2023-05-18 21:25:18 +0700

    tampilkan dari pilihan operator2 tsb.

--- 138_singlelinkedlist/138_singlelinkedlist.cpp
+++ 138_singlelinkedlist/138_singlelinkedlist.cpp
@@ -106,7 +106,7 @@ void traverse() {
 	}
 }
 
-void searcheData() {
+void searchData() {
 	if (listEmpty()) {
 		cout << "List Kosong" << endl;
 		system("pause");
@@ -128,3 +128,19 @@ void searcheData() {
 		cout << "Data tidak ditemukan" << endl;
 	}
 }
+
+int main() {
+	int pilihan;
+	do
+	{
+		try
+		{
+
+			cout << "1. Tambah Data" << endl;
+			cout << "2. Hapus Data" << endl;
+			cout << "3. Tampilkan Data" << endl;
+			cout << "4. Cari Data" << endl;
+			cout << "5. Keluar" << endl;
+			cout << "Pilihan: ";
+			cin >> pilihan;
+			
